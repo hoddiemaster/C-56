752c0684ada5a7a01b4ca7d0d7abf0b8faa81424
hoddiemaster <79085243+hoddiemaster@users.noreply.github.com>
2021-02-15 15:54:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function peru(){
+ window.alert("The Nazca Lines in southern Peru are a group of pre-Columbian geoglyphs etched into desert sands. Covering an area of nearly 1,000 sq. kilometers, there are about 300 different figures, including animals and plants. Composed of over 10,000 lines, some of which measure 30 meters wide and stretch more than 9 kilometers, the figures are most visible from the air or nearby hilltops."); 
+}
+function Italy(){
+window.alert("The Colosseum, is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum and is the largest ancient amphitheatre ever built, and is still the largest standing amphitheater in the world today.");  
+}
+function dheli(){
+window.alert("Often described as one of the wonders of the world, the stunning 17th Century white marble Taj Mahal was built by Mughal emperor Shah Jahan as a mausoleum for his beloved wife Mumtaz Mahal");  
+}
+function china(){
+window.alert("The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China as protection against various nomadic groups from the Eurasian Steppe.");  
+}
+function jordan(){
+window.alert("Petra is a famous archaeological site in Jordan's southwestern desert. Dating to around 300 B.C., it was the capital of the Nabatean Kingdom. Accessed via a narrow canyon called Al Siq, it contains tombs and temples carved into pink sandstone cliffs.A temple with an ornate, Greek-style facade, and known as The Treasury.");  
+} 
+function egypt(){
+window.alert("The Great Pyramid of Giza is the oldest and largest of the three pyramids in the Giza pyramid complex bordering present-day Giza in Greater Cairo, Egypt. It is the oldest of the Seven Wonders of the Ancient World, and the only one to remain largely intact.");
+}
+function london(){
+window.alert("The Lighthouse of Alexandria, sometimes called the Pharos of Alexandria, was a lighthouse built by the Ptolemaic Kingdom, during the reign of Ptolemy II Philadelphus, which has been estimated to be at least 100 metres in overall height.");
+}
